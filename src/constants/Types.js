92ecc8ba14a99d7cc81ca71bb8fc6d8099bf790f
jarosz <j.roszk@gmail.com>
2019-12-13 09:26:51 +0100

    Provide properties for Types (#109)

--- src/constants/Types.js
+++ src/constants/Types.js
@@ -1,4 +1,22 @@
-const Types = [
+// Extend Array to provide properties (syntactic sugar)
+// without breaking existing code, relying on Array interface
+// Types[0] == Types.SOLEMNITY and so on
+class TypesArray extends Array {
+  constructor(...items) {
+    // initialize Array
+    super(...items);
+
+    // iterate over all types and create dynamic read-only properties
+    for (var i = 0; i < this.length; ++i) {
+      Object.defineProperty(this, this[i], {
+        value: this[i],
+        writable: false
+      });
+    }
+  }
+}
+
+const Types = new TypesArray(
   "SOLEMNITY",          // [0]
   "SUNDAY",             // [1]
   "TRIDUUM",            // [2]
@@ -8,5 +26,5 @@ const Types = [
   "OPT_MEMORIAL",       // [6]
   "COMMEMORATION",      // [7]
   "FERIA"               // [8]
-];
+);
 export default Types;
